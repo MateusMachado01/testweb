f447c55704f2959d14651f1b3f410a90c744c076
MateusMachado01 <108525021+MateusMachado01@users.noreply.github.com>
2022-10-06 09:30:22 -0300

    dia testweb

---
+++ index.php
@@ -0,0 +1,15 @@
+
+    
+    
+    <?php
+    //INCLUDE DO ASIDE
+    include_once'./includes/_aside.php.;
+    include_once'./includes/_footer.php.;
+    ?>
+
+    <footer></footer>
+
+
+    <script src="./script.js"></script>
+</body>
+</html>
---
+++ noticia.php
@@ -0,0 +1,8 @@
+<?php
+include_once './includes/_header
+
+
+<?php
+include_once'./includes/_aside.php';
+include_once'./includes/_footer.php';
+?>
